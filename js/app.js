4acd2890a6912f789789210cca455b38d43ba019
Armando <jaimes087.uz@gmail.com>
2020-11-24 16:45:32 -0600

    Homepage finish, slider and responsive

--- js/app.js
+++ js/app.js
@@ -1,9 +1,15 @@
 const $btnNavOpen = document.getElementById('nav-mobile-open'),
-	$btnNavClose = document.getElementById('nav-mobile-close');
+	$btnNavClose = document.getElementById('nav-mobile-close'),
+	$btnSliderPrev = document.getElementById('slider-prev'),
+	$btnSliderNext = document.getElementById('slider-next'),
+	$slides = document.querySelectorAll('picture.hero__img'),
+	$slideInfo = document.querySelectorAll('.hero__info');
+
+let slideIndex = 0,
+	prevIndex = slideIndex;
 
 const openMenu = () => {
 	const handleMenuAnimation = ({ animationName }) => {
-		console.log(animationName);
 		navList.classList.remove(animationName);
 		if (animationName === 'out') {
 			nav.classList.remove('show');
@@ -24,6 +30,28 @@ const closeMenu = () => {
 	navList.classList.add('out');
 };
 
-$btnNavOpen.addEventListener('click', openMenu);
+const moveSlide = (prev, next) => {
+	$slides[next].classList.add('active');
+	$slideInfo[next].classList.add('active');
+	$slides[prev].classList.remove('active');
+	$slideInfo[prev].classList.remove('active');
+};
 
+const prevSlide = () => {
+	prevIndex = slideIndex;
+	--slideIndex;
+	slideIndex = slideIndex < 0 ? $slides.length - 1 : slideIndex;
+	moveSlide(prevIndex, slideIndex);
+};
+
+const nextSlide = () => {
+	prevIndex = slideIndex;
+	++slideIndex;
+	slideIndex = slideIndex === $slides.length ? 0 : slideIndex;
+	moveSlide(prevIndex, slideIndex);
+};
+
+$btnNavOpen.addEventListener('click', openMenu);
 $btnNavClose.addEventListener('click', closeMenu);
+$btnSliderPrev.addEventListener('click', prevSlide);
+$btnSliderNext.addEventListener('click', nextSlide);
